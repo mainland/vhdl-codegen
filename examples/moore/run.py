c6068c2203158e891e3c1013821bb9f32068a0aa
Geoffrey Mainland <mainland@apeiron.net>
2021-07-08 17:52:36 -0400

    Add Moore machine example.

---
+++ examples/moore/run.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from pathlib import Path
+from vunit import VUnit
+
+ROOT = Path(__file__).parent
+
+VU = VUnit.from_argv()
+
+VU.add_osvvm()
+LIB = VU.add_library("lib")
+LIB.add_source_files(ROOT / "rtl/incrementer.vhd")
+LIB.add_source_files(ROOT / "tb_incrementer.vhd")
+
+VU.main()
---
+++ examples/moore/src/Main.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Main (
+    main
+  ) where
+
+import Data.Fixed.Q
+import Data.Proxy ( Proxy )
+import Data.Sequence ( Seq )
+import Language.VHDL.Quote (ToType)
+import qualified Language.VHDL.Syntax as V
+
+import Language.VHDL.Codegen.Pipeline
+import Language.VHDL.Codegen.Pipeline.Testbench
+import Language.VHDL.Codegen.Monad
+import Language.VHDL.Codegen.VExp
+
+main :: IO ()
+main = do
+    (unit, p) <- evalCg $ mkIncrementer @(UQ 16 0)
+    writeDesignUnit "rtl/incrementer.vhd" unit
+    writeTestBench tbconf "tb_incrementer.vhd" p
+  where
+    tbconf = defaultTestBenchConfig { tb_entity   = "tb_incrementer"
+                                    , tb_watchdog = Just 50000
+                                    , tb_compare  = True
+                                    }
+
+mkIncrementer :: forall a m . (ToType (Proxy a), Num (VExp a), MonadCg m)
+              => m (Seq V.DesignUnit, Pipeline (VExp a) (VExp a))
+mkIncrementer = withDesignUnit $
+    moore "incrementer" (\x i -> x + i) (\x -> x) 0
